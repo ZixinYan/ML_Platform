8f24a5d6ecaf914d6c8cef31fd5e78f4e8b4f898
Yan Zixin <153819742+ZixinYan@users.noreply.github.com>
2025-08-11 22:47:10 +0800

    Revert "签到功能v0.8"

--- member/src/main/java/com/ml/member/dao/DaySignGrowthAwardDao.java
+++
@@ -1,9 +0,0 @@
-package com.ml.member.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ml.member.entity.DaySignGrowthAwardEntity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DaySignGrowthAwardDao extends BaseMapper<DaySignGrowthAwardEntity> {
-}
