2d460298999103b74a332a349b068bbd2805ae76
niangao555 <1650707281@qq.com>
2025-08-14 15:41:24 +0800

    签到功能v0.93.1：调整启动类引用

--- signin/src/main/java/com/ml/signIn/SignInApplication.java
+++ signin/src/main/java/com/ml/signIn/SignInApplication.java
@@ -1,6 +1,13 @@
 package com.ml.signIn;
 
+import com.ml.member.MemberApplication;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
 @MapperScan("com.ml.member.dao")
