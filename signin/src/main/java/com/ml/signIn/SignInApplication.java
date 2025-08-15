3e1a31df45f6b408cdc9c6a677be8d2b90c5c75b
niangao555 <1650707281@qq.com>
2025-08-15 14:48:02 +0800

    修改bug

--- signin/src/main/java/com/ml/signIn/SignInApplication.java
+++ signin/src/main/java/com/ml/signIn/SignInApplication.java
@@ -1,6 +1,5 @@
 package com.ml.signIn;
 
-import com.ml.member.MemberApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +16,6 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableScheduling
 public class SignInApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication.run(SignInApplication.class, args);
     }
 }
